832e5b84b17c4ee578f45678957db312c7b042fa
shrilakshmipv <28845000+shrilakshmipv@users.noreply.github.com>
2023-11-16 21:57:11 -0600

    update

--- ContainsDuplicates2.java
+++ ContainsDuplicates2.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 class ContainsDuplicates2{
     public boolean containsDuplicates(int[] nums){
         HashSet<Integer> unique = new HashSet<Integer>();
-        for(int i=0;i<nums.length;i++){
+        for(int i = 0;i < nums.length; i++){
             if(unique.contains(nums[i]))
                 return true;
 
@@ -14,11 +14,9 @@ class ContainsDuplicates2{
     }
 
     public static void main(String[] args){
-        ContainsDuplicates2 result= new ContainsDuplicates2();
+        ContainsDuplicates2 result = new ContainsDuplicates2();
         int[] nums_list = new int[]{1,2,3,4,5,6};
-        boolean res=result.containsDuplicates(nums_list);
+        boolean res = result.containsDuplicates(nums_list);
         System.out.println(res);
     }
 } 
-
-
